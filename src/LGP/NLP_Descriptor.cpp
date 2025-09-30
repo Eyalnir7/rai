be8709ee371a3c78a4338252c393f45ca63a69f7
Eyalnir7 <tadmoreyal@gmail.com>
2025-09-30 20:21:34 +0300

    changed the descriptor to put all the collisions in one constraint node

--- src/LGP/NLP_Descriptor.cpp
+++ src/LGP/NLP_Descriptor.cpp
@@ -11,6 +11,8 @@ rai::Graph getDescriptor(KOMO &komo, int verbose)
     cout << komo.report(true, true, false) << endl;
   }
   rai::Graph G;
+  rai::Array<rai::Node *> collFrames = {};
+  rai::Node *coll_node = nullptr;
   for (shared_ptr<Objective> &ob : komo.objectives)
   {
     if (ob->feat->frameIDs.N <= 2 && ob->feat->order <= 1)
@@ -28,6 +30,31 @@ rai::Graph getDescriptor(KOMO &komo, int verbose)
           o_name << '_' << go->feat->target;
         if (verbose > 1)
           cout << "\n  feature:" << o_name;
+        if (o_name.contains("PairCollision_ineq"))
+        {
+          continue;
+          coll_node = G.findNode(o_name);
+          if (!coll_node)
+          {
+            coll_node = G.add(o_name, true); // NEW FACTOR
+          }
+          for (rai::Frame *f : go->frames)
+          {
+            str f_name;
+            f_name << f->name << '_' << f->time;
+            if (verbose > 1)
+              cout << "\n  frame:" << f_name;
+            rai::Node *f_node = G.findNode(f_name);
+            if (!f_node)
+            {
+              str label = STRING("{pos:" << f->getPosition() << "}");
+              f_node = G.add<str>(f_name, label); // NEW VARIABLE
+              collFrames.append(f_node);
+            }
+          }
+          continue;
+        }
+
         rai::Node *o_node = G.add(o_name, true); // NEW FACTOR
 
         for (rai::Frame *f : go->frames)
@@ -39,7 +66,13 @@ rai::Graph getDescriptor(KOMO &komo, int verbose)
           rai::Node *f_node = G.findNode(f_name);
           if (!f_node)
           {
-            str label = STRING("{pos:" << f->getPosition() << "}");
+            rai::Array<double> size = {0., 0., 0.};
+            if(f->getShapeType() == rai::ST_ssBox){
+              size = f->getSize();
+              // get first three entries of the array
+              rai::Array<double> size = {size(0), size(1), size(2)};
+            }
+            str label = STRING("{pos:" << f->getPosition() << ", " << size << "}");
             f_node = G.add<str>(f_name, label); // NEW VARIABLE
           }
           o_node->addParent(f_node);
@@ -49,6 +82,10 @@ rai::Graph getDescriptor(KOMO &komo, int verbose)
       }
     }
   }
+  for (rai::Node *f : collFrames)
+  {
+    coll_node->addParent(f);
+  }
 
   if (verbose > 2)
   {
@@ -57,3 +94,58 @@ rai::Graph getDescriptor(KOMO &komo, int verbose)
 
   return G;
 }
+
+// rai::Graph getDescriptor(KOMO &komo, int verbose)
+// {
+//   if (verbose > 2)
+//   {
+//     LOG(0) << "=== Descriptor for following KOMO problem:";
+//     cout << komo.report(true, true, false) << endl;
+//   }
+//   rai::Graph G;
+//   for (shared_ptr<Objective> &ob : komo.objectives)
+//   {
+//     if (ob->feat->frameIDs.N <= 2 && ob->feat->order <= 1)
+//     {
+//       for (GroundedObjective *go : ob->groundings)
+//       {
+//         int timeSlice = go->timeSlices.elem(-1);
+
+//         if (verbose > 1)
+//           cout << "\n** objective " << *ob << ' ' << timeSlice;
+
+//         str o_name;
+//         o_name << go->feat->typeString() << '_' << go->type << go->feat->order;
+//         if (go->feat->target.N)
+//           o_name << '_' << go->feat->target;
+//         if (verbose > 1)
+//           cout << "\n  feature:" << o_name;
+//         rai::Node *o_node = G.add(o_name, true); // NEW FACTOR
+
+//         for (rai::Frame *f : go->frames)
+//         {
+//           str f_name;
+//           f_name << f->name << '_' << f->time;
+//           if (verbose > 1)
+//             cout << "\n  frame:" << f_name;
+//           rai::Node *f_node = G.findNode(f_name);
+//           if (!f_node)
+//           {
+//             str label = STRING("{pos:" << f->getPosition() << "}");
+//             f_node = G.add<str>(f_name, label); // NEW VARIABLE
+//           }
+//           o_node->addParent(f_node);
+//         }
+//         if (verbose > 1)
+//           cout << endl;
+//       }
+//     }
+//   }
+
+//   if (verbose > 2)
+//   {
+//     LOG(0) << "=== done";
+//   }
+
+//   return G;
+// }
